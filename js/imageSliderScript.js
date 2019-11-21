6b27ce6d31f7b2bc607741e25122d9d8c1368fcb
yoggie13 <ognjennikolic13@gmail.com>
2019-11-21 01:09:04 +0100

    Ubačeno Pekijevo

---
+++ js/imageSliderScript.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slike_kafe').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+});
