fd0d8f7d9c48ca3bff4e48e377d6ffa802c19606
booriiiis <57720077+booriiiis@users.noreply.github.com>
2019-11-21 17:00:38 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,14 @@
+var slider = document.getElementById('myRange');
+var output = document.getElementById('value');
+
+output.innerHTML = slider.getAttribute("value");
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+slider.addEventListener("mousemove", function() {
+    var x = slider.value;
+    var color = 'linear-gradient(90deg,rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
+    slider.style.background = color;
+})
