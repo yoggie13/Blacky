fdf2670a33310b7b69d0872afdbc312f9c3b98e0
yoggie13 <ognjennikolic13@gmail.com>
2019-11-21 19:38:13 +0100

    Sredjen order + izmenjen index

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,15 @@
 var slider = document.getElementById('myRange');
-var output = document.getElementById('value');
+var output = document.getElementById('price');
 
-output.innerHTML = slider.getAttribute("value");
+var gram = parseInt(slider.value);
+var price = Math.floor(gram / 200) * 360 + gram % 200 * 2;
+
+output.innerHTML = gram + "g = " + price + "din";
 
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    var gram = parseInt(this.value);
+    var price = Math.floor(gram / 200) * 360 + (gram % 200) * 2;
+    output.innerHTML = gram + "g = " + price + "din";
 }
 
 slider.addEventListener("mousemove", function() {
@@ -12,3 +17,7 @@ slider.addEventListener("mousemove", function() {
     var color = 'linear-gradient(90deg,rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
     slider.style.background = color;
 })
+
+function openForm() {
+
+}
