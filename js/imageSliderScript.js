14ddc5de859e87d2bcfe2e1579ac6f293e9ca4e1
yoggie13 <ognjennikolic13@gmail.com>
2019-11-25 02:00:11 +0100

    Krajnje izmene

--- js/imageSliderScript.js
+++ js/imageSliderScript.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     $('.slike_kafe').slick({
         dots: true,
         arrows: true,
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500
     });
 });
